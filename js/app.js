91d8a6ac94799e4c093b4e02d0ef69afb5d248ba
Cruzlutor <cruz.torrez@gmail.com>
2014-11-30 23:40:07 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+App = Ember.Application.create();
+
+App.Router.map(function() {
+	this.resource('articulo1');
+	this.resource('articulo2');
+	this.resource('articulo3');
+	this.resource('articulo4');
+	this.resource('articulo5');
+	this.resource('articulo6');
+	this.resource('articulo7');
+	this.resource('articulo8');
+	this.resource('articulo9');
+});
